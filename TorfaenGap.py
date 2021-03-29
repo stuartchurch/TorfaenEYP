44265c6cd7c9db20f9880b58926d21b0e6c6f462
stuartchurch <stuart.church@gmail.com>
2021-03-29 18:21:10 +0100

    Update TorfaenGap.py

--- TorfaenGap.py
+++ TorfaenGap.py
@@ -1,45 +1,56 @@
 import matplotlib.pyplot as plt 
 import pandas as pd
+import geopandas as gpd
 import numpy as np
 import streamlit as st
 
+
 #Load data
 df = pd.read_csv('TorfaenEI.csv')
-df=df.sort_values(['ServiceArea','MinAgeMo']) 
-size=df['ServiceName'].count()
+df=df.sort_values(['Minimum age in months'], ascending = False) 
+size=df['Intervention Name'].count()
 st.title("Torfaen Early Intervention : Gap Analysis")
 
-services = pd.DataFrame(df['ServiceArea'].unique())
-services.loc[-1] = ['All Service Areas']  # adding a row
+services = pd.DataFrame(df['Primary Domain/Need'].unique())
+services.loc[-1] = ['Show All']  # adding a row
 services.index = services.index + 1  # shifting index
 services = services.sort_index()  # sorting by index
 
-#Select a Service
-servicearea = st.selectbox('Select a Service Area:', services)
-st.subheader(servicearea)
+unitarspec = pd.DataFrame(df['Universal/Targeted/Specialist'].unique())
+unitarspec.loc[-1] = ['Show All']  # adding a row
+unitarspec.index = unitarspec.index + 1  # shifting index
+unitarspec = unitarspec.sort_index()  # sorting by index
 
+#Select a Service
+servicearea = st.selectbox('Select a Theme/Area:', services)
+unitarspec = st.selectbox('Select whether Universal, Targeted or Specialist:', unitarspec)
+
+if (servicearea != 'Show All') & (unitarspec != 'Show All'):	
+	df = df[(df['Primary Domain/Need']==servicearea) & (df['Universal/Targeted/Specialist']==unitarspec)]
+	size=df['Intervention Name'].count()
+
+if (servicearea != 'Show All') & (unitarspec == 'Show All'):	
+	df = df[(df['Primary Domain/Need']==servicearea)]
+	size=df['Intervention Name'].count()
+
+if (servicearea == 'Show All') & (unitarspec != 'Show All'):	
+	df = df[(df['Universal/Targeted/Specialist']==unitarspec)]
+	size=df['Intervention Name'].count()
+	
 fig, ax = plt.subplots()
 figure = plt.gcf()
-
-
-if servicearea != 'All Service Areas':	
-	df = df[df['ServiceArea']==servicearea]
-	size=df['ServiceName'].count()
-
 figure.set_size_inches(8,(size/2))
-
-#Programatically make colour dependent on Service Area
 plt.style.use('fivethirtyeight')
-plt.hlines(y=df['ServiceName'], xmin=df['MinAgeMo'], xmax=df['MaxAgeMo'], color=df['Color'], alpha=0.6)
-plt.scatter(df['MinAgeMo'], df['ServiceName'], color=df['Color'], alpha=1)
-plt.scatter(df['MaxAgeMo'], df['ServiceName'], color=df['Color'], alpha=1)
+plt.hlines(y=df['Intervention Name'], xmin=df['Minimum age in months'], xmax=df['Maximum age in months'], color='green', alpha=0.6)
+plt.scatter(df['Minimum age in months'], df['Intervention Name'], color='black', alpha=1)
+plt.scatter(df['Maximum age in months'], df['Intervention Name'], color='black', alpha=1)
 
 plt.xlabel('Age (Months)')
-plt.ylabel('Service Name')
+plt.ylabel('Intervention Name')
 
 st.pyplot(figure)
 
-	
+
 	
 	
 	
