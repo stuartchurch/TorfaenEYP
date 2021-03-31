11037451aa6f1e00eaa886a2d2715a6b83a6df16
stuartchurch <stuart.church@gmail.com>
2021-03-31 12:43:53 +0100

    Update TorfaenGap.py

--- TorfaenGap.py
+++ TorfaenGap.py
@@ -1,14 +1,28 @@
 import matplotlib.pyplot as plt 
 import pandas as pd
+import geopandas as gpd
 import numpy as np
 import streamlit as st
 
-
-#Load data
 df = pd.read_csv('TorfaenEI.csv')
-df=df.sort_values(['Minimum age in months'], ascending = False) 
+df = df.dropna( how='any', subset=['Minimum age in months', 'Maximum age in months'])
+df=df.sort_values(['Primary Domain/Need','Minimum age in months']) 
 size=df['Intervention Name'].count()
-st.title("Torfaen Early Intervention : Gap Analysis")
+
+#add new column for colour and make black by default
+df['Colour']='Red'
+
+lookupDict = {'Vulnerable families': 'Red', 
+	'Additional learning needs': 'Blue',
+	'Physical development and health': 'Green',
+	'Social and emotional': 'Brown',
+	'Cognitive development/Education': 'Grey',	
+	'Speech, language and communication': 'Violet'}
+
+mask = df['Primary Domain/Need'].isin(lookupDict.keys())
+df.loc[mask, 'Colour'] = df.loc[mask, 'Primary Domain/Need'].map(lookupDict)
+
+st.title("Torfaen Early Years Provision")
 
 services = pd.DataFrame(df['Primary Domain/Need'].unique())
 services.loc[-1] = ['Show All']  # adding a row
@@ -21,7 +35,7 @@ unitarspec.index = unitarspec.index + 1  # shifting index
 unitarspec = unitarspec.sort_index()  # sorting by index
 
 #Select a Service
-servicearea = st.selectbox('Select a Theme/Area:', services)
+servicearea = st.selectbox('Select a Domain/Need:', services)
 unitarspec = st.selectbox('Select whether Universal, Targeted or Specialist:', unitarspec)
 
 if (servicearea != 'Show All') & (unitarspec != 'Show All'):	
@@ -35,12 +49,12 @@ if (servicearea != 'Show All') & (unitarspec == 'Show All'):
 if (servicearea == 'Show All') & (unitarspec != 'Show All'):	
 	df = df[(df['Universal/Targeted/Specialist']==unitarspec)]
 	size=df['Intervention Name'].count()
-	
+
 fig, ax = plt.subplots()
 figure = plt.gcf()
 figure.set_size_inches(8,(size/2))
 plt.style.use('fivethirtyeight')
-plt.hlines(y=df['Intervention Name'], xmin=df['Minimum age in months'], xmax=df['Maximum age in months'], color='green', alpha=0.6)
+plt.hlines(y=df['Intervention Name'], xmin=df['Minimum age in months'], xmax=df['Maximum age in months'], color=df['Colour'], alpha=0.6)
 plt.scatter(df['Minimum age in months'], df['Intervention Name'], color='black', alpha=1)
 plt.scatter(df['Maximum age in months'], df['Intervention Name'], color='black', alpha=1)
 
