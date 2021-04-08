80067e42fcb96f47208bd6a4702df02d15ac86fb
stuartchurch <stuart.church@gmail.com>
2021-04-08 09:54:26 +0100

    Update TorfaenEYP.py

--- TorfaenEYP.py
+++ TorfaenEYP.py
@@ -1,23 +1,31 @@
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt  
 import pandas as pd
-import numpy as np
 import streamlit as st
+import requests
+from pandas import json_normalize
 
+#Load & cache data
 @st.cache
 def load_data():
-    data = pd.read_csv('TorfaenEI.csv')
-    return data
-
+	url = "https://v1.nocodeapi.com/essiesee/airtable/uYDVvsGUSfXBMszU?tableName=ServicesInterventions&view="
+	params = {}
+	r = requests.get(url = url, params = params)
+	result = r.json()
+	data = json_normalize(result['records'])
+	#remove fields. prefix from imported records
+	data.columns = data.columns.str.replace(r'fields.', '')
+	data = data[['Minimum age in months', 'Maximum age in months', 'Intervention Name', 'Primary Domain/Need','Universal/Targeted/Specialist']]
+	data = data.dropna(how='any', subset=['Minimum age in months', 'Maximum age in months'])
+	return data
 df = load_data()
 
-#df = pd.read_csv('TorfaenEI.csv')
-df = df.dropna( how='any', subset=['Minimum age in months', 'Maximum age in months'])
 df=df.sort_values(['Primary Domain/Need','Minimum age in months']) 
 size=df['Intervention Name'].count()
 
-#add new column for colour and make black by default
-df['Colour']='Red'
+#add new column for colour coding of domains/needs and make black by default
+df['Colour']='Black'
 
+#Dictionary to map domains to colours
 lookupDict = {'Vulnerable families': 'Red', 
 	'Additional learning needs': 'Blue',
 	'Physical development and health': 'Green',
@@ -30,11 +38,13 @@ df.loc[mask, 'Colour'] = df.loc[mask, 'Primary Domain/Need'].map(lookupDict)
 
 st.title("Torfaen Early Years Provision")
 
+#Identify unique domain names and an options for select all
 services = pd.DataFrame(df['Primary Domain/Need'].unique())
 services.loc[-1] = ['Show All']  # adding a row
 services.index = services.index + 1  # shifting index
 services = services.sort_index()  # sorting by index
 
+#Identify unique values for Universal/Targeted/Specialist and add option for select all
 unitarspec = pd.DataFrame(df['Universal/Targeted/Specialist'].unique())
 unitarspec.loc[-1] = ['Show All']  # adding a row
 unitarspec.index = unitarspec.index + 1  # shifting index
@@ -46,22 +56,22 @@ unitarspec = st.selectbox('Select whether Universal, Targeted or Specialist:', u
 
 st.image('key.png')
 
+#Subset dataframe based on values selected in dropdowns
 if (servicearea != 'Show All') & (unitarspec != 'Show All'):	
 	df = df[(df['Primary Domain/Need']==servicearea) & (df['Universal/Targeted/Specialist']==unitarspec)]
 	size=df['Intervention Name'].count()
-
 if (servicearea != 'Show All') & (unitarspec == 'Show All'):	
 	df = df[(df['Primary Domain/Need']==servicearea)]
 	size=df['Intervention Name'].count()
-
 if (servicearea == 'Show All') & (unitarspec != 'Show All'):	
 	df = df[(df['Universal/Targeted/Specialist']==unitarspec)]
 	size=df['Intervention Name'].count()
 
+#Plot lollipop chart, using fivethirtyeight theme        
+plt.style.use('fivethirtyeight')
 fig, ax = plt.subplots()
 figure = plt.gcf()
 figure.set_size_inches(8,(size/2))
-plt.style.use('fivethirtyeight')
 plt.hlines(y=df['Intervention Name'], xmin=df['Minimum age in months'], xmax=df['Maximum age in months'], color=df['Colour'], alpha=0.6)
 plt.scatter(df['Minimum age in months'], df['Intervention Name'], color='black', alpha=1)
 plt.scatter(df['Maximum age in months'], df['Intervention Name'], color='black', alpha=1)
@@ -69,7 +79,4 @@ plt.scatter(df['Maximum age in months'], df['Intervention Name'], color='black',
 plt.xlabel('Age (Months)')
 plt.ylabel('Intervention Name')
 
-st.pyplot(figure)
-
-
-
+st.pyplot(figure)	
